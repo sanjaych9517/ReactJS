42cf761fd9d8b020e6086518db47d2d77065af66
unknown <sanjaych9517@gmail.com>
2026-08-14 23:19:13 +0530

    add  color in buy button

--- 18_Fragments/learning-fragments/src/components/FoodItems.jsx
+++ 18_Fragments/learning-fragments/src/components/FoodItems.jsx
@@ -1,12 +1,22 @@
+import { useState } from "react";
 import Item from "./Item";
+
 const FoodItems = ({ items }) => {
+
+let [activeItem, setActiveItems] = useState([]);
+let onBuyButton = (item, event) =>{
+
+}
+
   return (
     <ul className="list-group">
       {items.map((item) => (
         <Item
           key={item}
           foodItem={item}
-          handleBuyButton={() => console.log(`${item} bought`)}
+          bought={activeItem.includes(item)}
+          handleBuyButton={(event) => onBuyButton(item, event)}
+
         ></Item>
       ))}
     </ul>
--- 18_Fragments/learning-fragments/src/components/Item.jsx
+++ 18_Fragments/learning-fragments/src/components/Item.jsx
@@ -1,12 +1,12 @@
 import styles from "./Item.module.css";
-const Item = ({ foodItem, handleBuyButton }) => {
+const Item = ({ foodItem, bought, handleBuyButton }) => {
   const handleBuyButtonClicked = (event) => {
     console.log(event)
     console.log(`${foodItem} being bought`);
   };
 
   return (
-    <li className={`${styles[`kg-item`]} list-group-item`}>
+    <li className={`${styles[`kg-item`]} list-group-item ${bought && 'active'}`}>
       <span className={`${styles[`kg-span`]}`}>{foodItem}</span>
       <button
         className={`${styles.button} btn btn-info`}
