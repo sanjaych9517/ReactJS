da9522c84d6e1bdcc3d2eb00fab7062d3950cb9f
unknown <sanjaych9517@gmail.com>
2026-08-16 23:11:38 +0530

    we create header of my website

--- Projects/7_socialMedia/src/App.jsx
+++ Projects/7_socialMedia/src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Header from "./component/Header";
 
 function App() {
-
   return (
     <>
-      
+      <Header></Header>
     </>
   );
 }
