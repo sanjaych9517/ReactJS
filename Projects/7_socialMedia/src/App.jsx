aea8acb4d1a5a826440d34018d7bb0c31431f003
unknown <sanjaych9517@gmail.com>
2026-08-16 23:21:59 +0530

    we add fotter in our website

--- Projects/7_socialMedia/src/App.jsx
+++ Projects/7_socialMedia/src/App.jsx
@@ -1,11 +1,15 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Header from "./component/Header";
+import Fotter from "./component/Fotter";
+import Sidebar from "./component/Sidebar";
 
 function App() {
   return (
     <>
       <Header></Header>
+
+      <Fotter></Fotter>
     </>
   );
 }
