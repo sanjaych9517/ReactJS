d5a7c35834349c0adced48c1a3eba566ef1ee36d
unknown <sanjaych9517@gmail.com>
2026-08-17 15:19:57 +0530

    we add form for create post

--- Projects/7_socialMedia/src/App.jsx
+++ Projects/7_socialMedia/src/App.jsx
@@ -4,12 +4,15 @@ import Header from "./component/Header";
 import Fotter from "./component/Fotter";
 import Sidebar from "./component/Sidebar";
 
+import CreatePost from "./component/CreatePost";
+
 function App() {
   return (
     <div className="app-container">
       <Sidebar></Sidebar>
       <div className="content">
         <Header></Header>
+       <CreatePost></CreatePost>
         <Fotter></Fotter>
       </div>
     </div>
