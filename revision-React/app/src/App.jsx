86c3d3ff529cf8fa9787320da78e3c00cccbea24
unknown <sanjaych9517@gmail.com>
2026-09-02 19:13:49 +0530

    we learn props

--- revision-React/app/src/App.jsx
+++ revision-React/app/src/App.jsx
@@ -1,35 +1,15 @@
-import React from "react";
-
-
-function Component2() {
-  return(
-    <>
-    <img src="https://i.mdel.net/i/db/thumbs/otm/400x/2026/08/mathilde.jpg" alt="" />
-    </>
-  )
-}
-function Component() {
-  return (
-    <div>
-      <Component2 />
-      <h1>Sanjay kapoor</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Alias,
-        perspiciatis molestiae ratione, reiciendis aperiam ullam dignissimos
-        cupiditate recusandae, ex odio corporis. Praesentium magnam nostrum
-        placeat vero ea nemo maxime natus.
-      </p>
-    </div>
-  );
-}
+import React from 'react'
+import Navbar from './navbar'
 
 const App = () => {
   return (
     <div>
-      <Component />
-      <Component />
+      <Navbar title="name" link="google.com"/>
+       <Navbar title="Sanjay" link="amazon.com"/>
+        <Navbar number={4} />
+        <Navbar title="kapoor ji"/>
     </div>
-  );
-};
+  )
+}
 
 export default App;
---
+++ revision-React/app/src/navbar.jsx
@@ -0,0 +1,13 @@
+export function Navbar({title, number, link}) {
+    
+  return (
+    <div>
+      <h1>{title}</h1>
+      <h1>{link}</h1>
+      <h1>{number}</h1>
+    </div>
+  )
+}
+
+export default Navbar;
+
