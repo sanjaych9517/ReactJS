30c99faf4158ec84ceaeca27a9c5f01640452fa8
unknown <sanjaych9517@gmail.com>
2026-09-03 16:05:49 +0530

    welearn import export in react

--- revision-React/02_setup/src/App.jsx
+++ revision-React/02_setup/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   
 
   return (
-   <h1>Hellow worlss</h1>
+   <h1>Hellow  worlss</h1>
   )
 }
 
---
+++ revision-React/06_Import/app/src/App.jsx
@@ -0,0 +1,15 @@
+import Header from './Header'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>App </h1>
+     <Header/>
+    </>
+  )
+}
+
+export default App;
