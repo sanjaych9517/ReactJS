fa1bfa9ba8c9f863a767eb3ee3d40a5f2486ac06
unknown <sanjaych9517@gmail.com>
2026-09-04 07:30:03 +0530

    we learn problem of css which handle by tailwind css

---
+++ revision-React/10_tailwind/app/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+// import Card from './Card'
+
+function App() {
+  return (
+<>
+<h1>kaise ho bhai  log</h1>
+{/* <Card/> */}
+<p className='card'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Repellendus quas illum, adipisci quos laboriosam totam libero error reiciendis soluta cumque ea numquam molestiae excepturi ipsa deserunt non earum provident nisi!</p>
+</>
+  )
+}
+
+export default App
---
+++ revision-React/10_tailwind/app/src/Card.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Card.css'
+const Card = () => {
+  return (
+    <h1 className='card'>Card</h1>
+  )
+}
+
+export default Card
