627db6219572894e228aa35379c88eba5407a269
unknown <sanjaych9517@gmail.com>
2026-09-08 05:24:51 +0530

    we learn basic handling of radio button with state

---
+++ revision-React/25_HandleRadioButton/app/src/notes/Basic.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react";
+
+function App() {
+  const [gender, setGender] = useState('');
+console.log(gender);
+  return (
+    <>
+      {/* MALE */}
+      <label htmlFor="male">
+        <input
+          type="radio"
+          id="male"
+          name="gender"
+          value={"male"}
+          checked={gender === "male"}
+          onChange={(e) => {
+            setGender(e.target.value);
+          }}
+        />
+        Male
+      </label>
+      <br />
+      <br />
+      {/* FEMALE */}
+
+      <label htmlFor="female">
+        <input
+          type="radio"
+          id="female"
+          name="gender"
+          value={"female"}
+          checked={gender === "female"}
+          onChange={(e) => {
+            setGender(e.target.value);
+          }}
+        />
+        Female
+      </label>
+
+      {/* other */}
+      <br />
+      <br />
+      <label htmlFor="other">
+        <input
+          type="radio"
+          id="other"
+          name="gender"
+          value={"other"}
+          checked={gender === "other"}
+          onChange={(e) => {
+            setGender(e.target.value);
+          }}
+        />
+        Other
+      </label>
+    </>
+  );
+}
+
+export default App;
