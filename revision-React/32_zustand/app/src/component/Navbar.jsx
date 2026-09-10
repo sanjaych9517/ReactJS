1b1d835c18249d96297c57b2d154a7366ddb8f42
unknown <sanjaych9517@gmail.com>
2026-09-10 19:28:27 +0530

    we learn set and get function in zustand

--- revision-React/32_zustand/app/src/component/Navbar.jsx
+++ revision-React/32_zustand/app/src/component/Navbar.jsx
@@ -3,16 +3,21 @@ import useMyStore from "../store";
 
 const Navbar = () => {
   const state = useMyStore();
-  console.log(state)
-const handleClick = () => {
-  state.increment();
-};
+  console.log(state);
+  const handleClick = () => {
+    state.increment();
+  };
+
+  const handleNameClick = () => {
+    state.capitalizeName();
+  };
 
   return (
     <div>
       <p>{state.name}</p>
       <p>{state.count}</p>
-      <button onClick={handleClick}>increment</button>
+      <button onClick={handleClick}>increment</button> <br /> <br />
+      <button onClick={handleNameClick}>capitilize</button>
     </div>
   );
 };
--- revision-React/32_zustand/app/src/store.js
+++ revision-React/32_zustand/app/src/store.js
@@ -1,13 +1,21 @@
 import { create } from "zustand";
 
-const useMyStore = create((set) => ({
-    name: "Sanjay",
+const useMyStore = create((set, get) => ({
+    name: "sanjay",
     count: 0,
 
-    increment: () =>
+    increment: () => {
         set((state) => ({
             count: state.count + 1,
-        })),
+        }))
+    },
+    capitalizeName: () => {
+        const { name } = get();
+        set({
+            name: name.charAt(0).toUpperCase() + name.slice(1)
+        })
+        
+    }
 }));
 
 export default useMyStore;
